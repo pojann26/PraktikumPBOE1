5f99002deb8ac696b07c278f8213bff84a2133d8
pojann26 <dezanaja2006@gmail.com>
2026-02-27 11:10:05 +0700

    Pertemuan 2

--- Pertemuan_2/src/MTitik.java
+++ Pertemuan_2/src/MTitik.java
@@ -1,28 +1,65 @@
-/* Nama File    : MTitik.java
- * Deskripsi    : berisi atribut dan method dalam class Titik
- * Pembuat      : Muhammad Fauzan Akbar
- * Tanggal      : 27 Februari 2026
+/* Nama File : MTitik.java
+ * Deskripsi : berisi main class untuk menguji class Titik
+ * Pembuat   : Muhammad Fauzan Akbar
+ * Tanggal   : 27 Februari 2026
  */
 
 public class MTitik {
     public static void main(String[] args) {
-        Titik T1 = new Titik(); //Membuat objek titik T1 (0,0)
-        T1.setAbsis(3);         //mengubah absis T1 dengan nilai 3
-        T1.setOrdinat(4);       //mengubah ordinat T1 dengan nilai 4
-        T1.printTitik();        //mencetak koordinat T1 ke layar
-        T1.geser(3,4);          //menggeser T1 sejauh (3,4)
-        T1.printTitik();        //menampilkan koordinat T1 setelah digeser
+        Titik T1 = new Titik(); // Membuat objek titik T1 (0,0)
+        T1.setAbsis(3);         // mengubah absis T1 dengan nilai 3
+        T1.setOrdinat(4);       // mengubah ordinat T1 dengan nilai 4
+        T1.printTitik();        // mencetak koordinat T1 ke layar
+        T1.geser(3, 4);         // menggeser T1 sejauh (3,4)
+        T1.printTitik();        // menampilkan koordinat T1 setelah digeser
 
-        Titik T2 = new Titik(3,5);
+        Titik T2 = T1; 
         T2.printTitik();
         T1.setAbsis(10);
         T1.setOrdinat(10);
         T2.printTitik();
 
-        
-        System.out.println("Jumlah Objek Titik = " + Titik.getCounterTitik());
-        System.out.println("Jumlah Objek Titik = " + T2.getCounterTitik());
+        Titik T3 = new Titik(3, 5);
+        System.out.println("Jumlah objek titik = " + Titik.getCounterTitik());
+        System.out.println("Jumlah objek titik = " + T3.getCounterTitik());
 
-        T1.printCounterTitik();
-    }
+        // uji getKuadran 
+        Titik t1 = new Titik(10, 15); // Kuadran I
+        System.out.println("Kuadran t1: " + t1.getKuadran());
+
+        Titik t2 = new Titik(-10, 15); // Kuadran II
+        System.out.println("Kuadran t2: " + t2.getKuadran());
+
+        Titik t3 = new Titik(-10, -15); // Kuadran III
+        System.out.println("Kuadran t3: " + t3.getKuadran());
+
+        Titik t4 = new Titik(10, -15); // Kuadran IV
+        System.out.println("Kuadran t4: " + t4.getKuadran());
+
+        // uji getjarakPusat
+        System.out.println("Jarak t1 ke pusat: " + t1.getjarakPusat());
+
+        // uji getJarak antara t1 dan t2
+        System.out.println("Jarak antara t1 dan t2: " + t1.getJarak(t2));
+
+        // uji refleksiX
+        t1.refleksiX();
+        System.out.print("t1 setelah refleksiX: ");
+        t1.printTitik();
+
+        // uji refleksiY
+        t1.refleksiY();
+        System.out.print("t1 setelah refleksiY: ");
+        t1.printTitik();
+
+        // uji getRefleksiX
+        Titik t1RefleksiX = t1.getRefleksiX();
+        System.out.print("getRefleksiX dari t1: ");
+        t1RefleksiX.printTitik();
+
+        // uji getRefleksiY
+        Titik t1RefleksiY = t1.getRefleksiY();
+        System.out.print("getRefleksiY dari t1: ");
+        t1RefleksiY.printTitik();
+    }    
 }
--- Pertemuan_2/src/Titik.java
+++ Pertemuan_2/src/Titik.java
@@ -64,4 +64,49 @@ public class Titik {
     void printCounterTitik(){
         System.out.println(this.counterTitik);
     }
+
+     // mendapatkan kuadran titik
+  public int getKuadran() {
+    if (absis > 0 && ordinat > 0) {
+      return 1;
+    } else if (absis < 0 && ordinat > 0) {
+      return 2;
+    } else if (absis < 0 && ordinat < 0) {
+      return 3;
+    } else if (absis > 0 && ordinat < 0) {
+      return 4;
+    } else {
+      return 0; // titik berada di pusat koordinat
+    }
+  }
+
+  // mendapatkan jarak titik ke pusat koordinat
+  public double getjarakPusat() {
+    return Math.sqrt(absis * absis + ordinat * ordinat);
+  }
+
+  // mendapatkan jarak titik ke titik T
+  public double getJarak(Titik T) {
+    return Math.sqrt((absis - T.absis) * (absis - T.absis) + (ordinat - T.ordinat) * (ordinat - T.ordinat));
+  }
+
+  // refleksi terhadap sumbu x
+  public void refleksiX() {
+    ordinat = -ordinat;
+  }
+
+  // refleksi terhadap sumbu y
+  public void refleksiY() {
+    absis = -absis;
+  }
+
+  // mengembalikan titik hasil refleksi terhadap sumbu x
+  public Titik getRefleksiX() {
+    return new Titik(absis, -ordinat);
+  }
+
+  // mengembalikan titik hasil refleksi terhadap sumbu y
+  public Titik getRefleksiY() {
+    return new Titik(-absis, ordinat);
+  }
 } //end class Titik
