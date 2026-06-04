8bc0c65bc4b15b4c9769e2b9db2f9bb3304a7fbc
pojann26 <dezanaja2006@gmail.com>
2026-06-04 18:38:39 +0700

    Pertemuan 12 - Ekspresi Lambda

---
+++ Pertemuan_12/DiskonLambda.java
@@ -0,0 +1,30 @@
+package Pertemuan_13;
+
+// File: DiskonLambda.java
+// Deskripsi: Ekspresi lambda dasar, digunakan untuk menghitung diskon.
+
+interface IDiskon {
+    public double hitungDiskon(int harga);
+}
+
+public class DiskonLambda {
+    public static void main(String[] args) {
+        // 1. Tanpa lambda (Menggunakan Anonymous Inner Class)
+        IDiskon diskonMerdeka = new IDiskon() {
+            @Override
+            public double hitungDiskon(int harga) {
+                return harga - (harga * 0.3);
+            }
+        };
+        // 2. Dengan lambda (Satu baris ekspresi, tanpa return keyword)
+        IDiskon diskonLebaran = (harga) -> harga - (harga * 0.4);
+        // 3. Dengan lambda dengan blok statement (Menggunakan kurung kurawal & return)
+        IDiskon diskonBiasa = (harga) -> {
+            return harga - (harga * 0.1);
+        };
+
+        System.out.println("Diskon Merdeka: " + diskonMerdeka.hitungDiskon(45000));
+        System.out.println("Diskon Lebaran: " + diskonLebaran.hitungDiskon(45000));
+        System.out.println("Diskon Biasa: " + diskonBiasa.hitungDiskon(45000));
+    }
+}
---
+++ Pertemuan_12/LambdaList.java
@@ -0,0 +1,19 @@
+package Pertemuan_13;
+
+// File: LambdaList.java
+// Deskripsi: Implementasi lambda pada List, digunakan sebagai parameter pada method.
+
+import java.util.ArrayList;
+
+public class LambdaList {
+    public static void main(String[] args) {
+        ArrayList<String> mahasiswaList = new ArrayList<>();
+        mahasiswaList.add("Adi");
+        mahasiswaList.add("Bambang");
+        mahasiswaList.add("Cici");
+        mahasiswaList.add("Didi");
+
+        // Lambda digunakan sebagai parameter pada method forEach
+        mahasiswaList.forEach((nama) -> System.out.println(nama));
+    }
+}
